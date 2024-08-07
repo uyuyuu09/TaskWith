bc4adc97b088501b5bb21a93095304bd432fc08e
uyuyuu09 <220109@oks.city-saitama.ed.jp>
2024-08-07 10:14:57 +0900

    CHANGE: CSS, default.vue, package-lock.json

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,12 +1,9 @@
-<script setup lang="ts">
-</script>
-
 <template>
         <header class="header text-gray-600 body-font sticky top-0">
             <div class="mx-auto flex flex-wrap p-2 md:flex-row items-center">
                 <div class="items-center my-1 ">
                     <section>
-                        <img class="mypass_text" src="~/public/TaskWith.webp" alt="TaskWIth ~Every Day With You~" id="logo">
+                        <img class="taskwith_logo" src="~/public/TaskWith.webp" alt="TaskWIth ~Every Day With You~" id="logo">
                     </section>
                 </div>
             </div>
