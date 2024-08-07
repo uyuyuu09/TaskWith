985a5a17a912f934047f8a3a6ea3b58a2ca300a8
uyuyuu09 <220109@oks.city-saitama.ed.jp>
2024-08-08 01:56:54 +0900

    CHANGE: userストアの有効期限を1時間に変更

--- frontend/composables/auth.ts
+++ frontend/composables/auth.ts
@@ -14,13 +14,10 @@ export const useUserStore = defineStore('user', {
     actions: {
         async login(email: string, password: string) {
             try {
-                // const { accessToken, userData } = await callLogin(email, password) as { accessToken: string, userData: any };
                 const { token, userData } = await callLogin(email, password) as { token: string, userData: any };
                 if (token) {
-                    // console.log("token:" + token)
                     this.isLoggedIn = true;
                     this.user = userData;
-                    // setCookie('accessToken', accessToken);
                     console.log("auth:" + token, userData.email)
                     return { token, userData }
                 } else {
@@ -39,28 +36,7 @@ export const useUserStore = defineStore('user', {
     persist: {
         storage: persistedState.cookiesWithOptions({
             sameSite: 'strict',
-            maxAge: 604800,
+            maxAge: 60 * 60,
         }),
     },
 });
-
-// export const useUserStore = defineStore('user',
-//     () => {
-//         const token = ref<string | null>(null);
-//         function setToken(new_user: string) {
-//             token.value = new_user;
-//         }
-//         function clearToken() {
-//             token.value = null;
-//         }
-//         return { token, setToken, clearToken }
-//     },
-//     {
-//         persist: {
-//             storage: persistedState.cookiesWithOptions({
-//                 sameSite: 'strict',
-//                 maxAge: 604800,
-//             }),
-//         },
-//     }
-// );
