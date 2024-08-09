223261ebde5b6d2bbba6d0003494f263d6d2bff4
uyuyuu09 <220109@oks.city-saitama.ed.jp>
2024-08-09 21:15:02 +0900

    CHANGE: Cookieの保持時間

--- frontend/composables/cookie.ts
+++ frontend/composables/cookie.ts
@@ -1,6 +1,6 @@
 export function setCookie(name: string, value: string) {
     const date = new Date();
-    date.setTime(date.getTime() + 3600 * 1000); //3600000[ms]
+    date.setTime(date.getTime() + 3600 * 1000 * 3); //3600000[ms]
     const cookieExpires = "expires=" + date.toUTCString();
     document.cookie = `${name}=${value}; ${cookieExpires}; path=/`;
 }
