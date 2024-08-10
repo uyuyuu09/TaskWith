82d387a8b837517c1f08dd7151bdeba9c0084afb
uyuyuu09 <220109@oks.city-saitama.ed.jp>
2024-08-10 14:23:55 +0900

    ADD: 予定(イベント)の取得機能

--- frontend/composables/events.ts
+++ frontend/composables/events.ts
@@ -20,3 +20,18 @@ export async function create_events(event_name: string, start_time: string, end_
     }
     return
 }
+
+export async function get_all_events() {
+    const token = String(getCookie("token"))
+    const get_all_events = await fetch("http://192.168.0.41:8000/events/get", {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    if (!get_all_events.ok) {
+        throw new Error(`HTTP error status: ${get_all_events.status}`);
+    }
+    const events = await get_all_events.json()
+    return events
+}
